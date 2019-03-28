8066f93342cb8ce7c2ad54f3b2869c06f28d5997
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-03-28 22:24:39 +0530

    add functions to make the facility for upload the user profile picture

--- app/src/main/java/com/example/myapplication/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/RegisterActivity.java
@@ -1,15 +1,89 @@
 package com.example.myapplication.Activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView ImgUserPhoto;
+    static int PReqCode = 1;
+    static int REQUESCODE = 1;
+    Uri pickedImgUri;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //inu views
+        ImgUserPhoto = findViewById(R.id.regUserPhoto);
+
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(Build.VERSION.SDK_INT>= 22){
+                    checkAndRequestForPermission();
+                }
+                else{
+                    openGallery();
+                }
+            }
+        });
+    }
+
+    private void openGallery(){
+        //TODO: open gallery internt and wait for user to pick an image
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESCODE);
+
+    }
+
+    private void checkAndRequestForPermission(){
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE )!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(RegisterActivity.this, "pleasr accep for required permission", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                                    PReqCode);
+            }
+        }
+        else{
+            openGallery();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == REQUESCODE && data != null){
+
+            //the user as successful picked on image
+            //we need save its reference to a uri
+
+            pickedImgUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImgUri);
+
+        }
+
     }
 }
